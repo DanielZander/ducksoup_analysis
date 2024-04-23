8d3fd0d8d8f8ef7e7b0f171995fd3839f9a5be18
DanielZander <da8183za-s@student.lu.se>
2024-04-23 14:14:58 +0200

    more details to main script

--- main.R
+++ main.R
@@ -12,7 +12,7 @@ df = read_csv("clean_data.csv ")
 
 df$unfair = ifelse(df$sent_amount >= 5, 1, 0)
 df$manipulation = ifelse(df$manipulation == 1, "dom", "sub")
-df$other_manip = ifelse(df$manipulation == "dom", "sub", "dom")
+df$other_manip = ifelse(df$manipulation == "dom", "sub", "dom") #Manipulation of other player in group.
 
 
 ################################################################################
@@ -21,7 +21,42 @@ df$other_manip = ifelse(df$manipulation == "dom", "sub", "dom")
 #acceptance rate and stuff
 1-mean(df$unfair, na.rm = TRUE) #37.9% of offers were unfair, i.e. <5 coins. 
 
-mean(df$offer_response, na.rm = TRUE) #0.9272727
+mean(df$offer_response, na.rm = TRUE) #92.72727% of offers are accepted.
+aggregate(offer_response~unfair,df, mean)
+#     unfair         offer_response
+#1      0              0.8095238
+#2      1              1.0000000
+
+resp_df = df[df$role == "responder", ]
+#acceptance rate of responders depending on the manipulation of proposer voice.
+aggregate(offer_response ~ other_manip, resp_df, mean)
+#     other_manip   offer_response
+#1         dom        0.9285714
+#2         sub        0.9259259
+
+aggregate(offer_response ~ other_manip, resp_df, sum)
+#       other_manip     offer_response
+#1         dom              26
+#2         sub              25
+
+
+# 1 = fair, 0 = unfair.
+aggregate(offer_response ~ unfair + other_manip, resp_df ,mean)
+#     unfair    other_manip    offer_response
+#1      0           dom         0.8181818
+#2      1           dom         1.0000000
+#3      0            sub        0.8000000
+#4      1           sub         1.0000000
+
+# 1 = fair, 0 = unfair.
+aggregate(offer_response ~ unfair + other_manip, resp_df ,sum)
+#     unfair    other_manip     offer_response
+#1      0           dom                9
+#2      1           dom                17
+#3      0           sub                8
+#4      1           sub                17
+
+
 
 #PROPOSER SENT AMOUNT
 mean(df$sent_amount, na.rm = TRUE) #4.551724
@@ -37,23 +72,35 @@ ggplot(means, aes(round_nb, mean_sent_amount))+
   geom_hline(yintercept = mean(df$sent_amount, na.rm = TRUE), color = "red", linetype = 2, size = 1)
 
 
-# Calculate mean of sent_amount by player
-means <- df %>%
-  group_by(player) %>%
-  summarise(mean_sent_amount = mean(sent_amount))
-#plot
-ggplot(means, aes(player, mean_sent_amount))+
-  geom_col(color = "black")+
-  geom_hline(yintercept = mean(df$sent_amount, na.rm = TRUE), color = "red", linetype = 2, size = 1)
-
-
-
 #TRIAL PAYOFF
-mean(df$sent_amount, na.rm = TRUE) #4.551724
 trial_payoff_mean = mean(df$trial_payoff)#4.25
 sd(df$trial_payoff) #1.9
 
+only_accept_df = df[df$offer_response == 1, ]
+ggplot(only_accept_df, aes(trial_payoff))+
+  geom_histogram(binwidth = 1, color = "black")
 
+# Calculate mean of trial_payoff by player
+means <- df %>%
+  group_by(player) %>%
+  summarise(trial_payoff_mean = mean(trial_payoff))
+sd <- df %>%
+  group_by(player) %>%
+  summarise(trial_payoff_sd = sd(trial_payoff))
+plot1_df = data.frame(trial_payoff_mean = means, trial_payoff_sd = sd)
+
+#plot
+ggplot(plot1_df, aes(trial_payoff_mean.player,trial_payoff_mean.trial_payoff_mean ))+
+  geom_col(color = "black")+
+  geom_hline(yintercept = mean(df$trial_payoff, na.rm = TRUE), color = "red", linetype = 2, size = 1)+
+  geom_errorbar(aes(ymin = trial_payoff_mean.trial_payoff_mean  - trial_payoff_sd.trial_payoff_sd, ymax = trial_payoff_mean.trial_payoff_mean  + trial_payoff_sd.trial_payoff_sd), width = 0.2, color = "blue")+
+  scale_y_continuous(limits = c(0, 10))
+
+df_total_payoff = aggregate(trial_payoff ~ player, df, sum)
+ggplot(df_total_payoff, aes(player, trial_payoff))+
+  geom_col()
+
+#What is special about player 1 and player 2?
 
 #Trial_payoff grouped according to manipulation
 aggregate(trial_payoff ~ manipulation, df, mean)
@@ -61,6 +108,10 @@ aggregate(trial_payoff ~ manipulation, df, mean)
 #1          dom         4.283333
 #2          sub         4.216667
 
+
+aggregate(trial_payoff ~ role, df, mean)
+
+#Prediction 1: Responders will agree to receive less amount of money when faced with a vocally dominant Proposer. 
 #Grouped according to both manip and role
 aggregate(trial_payoff ~ manipulation + role, df , mean)
 #      manipulation      role       trial_payoff
@@ -69,57 +120,48 @@ aggregate(trial_payoff ~ manipulation + role, df , mean)
 #3          dom        responder      3.933333
 #4          sub        responder      4.033333
 
+
 # Calculate mean of trial_payoff grouped by manipulation and role
 means <- df %>%
   group_by(manipulation, role) %>%
   summarise(mean_trial_payoff = mean(trial_payoff))
+
+sd <- df %>%
+  group_by(manipulation, role) %>%
+  summarise(sd_trial_payoff = sd(trial_payoff))
 # Plot
 ggplot(data = means, aes(x = manipulation, y = mean_trial_payoff, fill = role)) +
   geom_col(position = position_dodge(width = .5), color = "black") +
   labs(x = "Manipulation", y = "Mean trial_payoff") +
   theme_bw()+
   scale_y_continuous(limits = c(0, 5))+
-  geom_hline(yintercept = trial_payoff_mean, color = "red", linetype = 2, size = 1.2)+
-  scale_fill_manual(values = c("blue", "green"))
-
-
+  geom_hline(yintercept = mean(df$trial_payoff), color = "red", linetype = 2, size = 1.2)+
+  scale_fill_manual(values = c("darkgreen", "darkolivegreen1"))
 
 
 #Reaction time 
 mean(df$rt, na.rm = TRUE) #6140ms 
 sd(df$rt, na.rm = TRUE) #5275ms
-hist(df$rt)
+subset_df <- subset(df, rt <= 30000)
+# Plot the histogram
+hist(subset_df$rt)
+ggplot(subset_df, aes(rt))+
+  geom_histogram(color = "black")+
+  scale_x_continuous(breaks = seq(0, 30000, by = 2000))  
 
-resp_df = df[df$role == "responder", ]
 aggregate(rt ~ other_manip, resp_df, mean)
-aggregate(trial_payoff ~ manipulation + role, df, mean)
 #       other_manip       rt
 #1         dom          6255.813
 #2         sub          5202.975
 
-
-
-df_dominant = df[df$role == "proposer",]
-mean(df_dominant$sent_amount, na.rm = TRUE)
-sum(df_dominant$offer_response, na.rm = TRUE )
-
-aggregate(offer_response ~ unfair_1 + manipulation, df ,mean)
-
-
-df_sub = df[df$role == "responder",]
-df_sub$dom_proposer = ifelse(df_sub$manipulation == "sub", "prop_dom", "prop_sub")
-aggregate(offer_response ~ dom_proposer + unfair, df_sub, sum)
+x = lm(rt ~ other_manip, resp_df)
+summary(x)
 
 
 
 
-aggregate(offer_response ~ manipulation, df_sub, sum)
 
 
-#     dom_proposer          unfair          offer_response
-#1     prop_dom         fairorbetter              17
-#2     prop_sub         fairorbetter              17
-#3     prop_dom             unfair                9
-#4     prop_sub             unfair                8
+
 
 
