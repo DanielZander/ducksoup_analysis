bfc34cbd20d951ec51478abcebc50d1fd5c4f9f6
DanielZander <da8183za-s@student.lu.se>
2024-05-02 11:54:52 +0200

    handling of different data sets, combined dfs.

--- preprocessing.py
+++ preprocessing.py
@@ -6,10 +6,11 @@ import os
 from statistics import mean
 
 # %%
+data_name = "data_mk4_080ps8pg"
+
 currentDir = os.getcwd()
-csvPath = os.path.join(currentDir, 'raw_data\data.csv')
+csvPath = os.path.join(currentDir, f'raw_data\{data_name}.csv')
 print(csvPath)
-columnsInterest = ['participant.code', ]
 raw_data = pd.read_csv(csvPath)
 
 column_names = ["sid","session_code", "mk_session","prolific_id","participant_code", "round_nb", "role","player", "dyad", "manipulation",
@@ -22,6 +23,7 @@ column_debrief = ["sid","session_code", "mk_session", "prolific_id", "participan
 clean_data_debrief = pd.DataFrame(columns=column_debrief)
 
 # %%
+
 for subject in raw_data['participant.id_in_session']:
     subject_row = (raw_data[raw_data['participant.id_in_session'] == subject])
     
@@ -34,16 +36,16 @@ for subject in raw_data['participant.id_in_session']:
             
         row_data = {
             'participant_code': subject_row['participant.code'].values[0],
-            'session_code': subject_row['session.conde'].values[0],
+            'session_code': subject_row['session.code'].values[0],
             'prolific_id': subject_row['ultimatum_game.20.player.prolific_id'].values[0],
             'sid': subject_row['session.config.name'].values[0],
             'mk_session': subject_row['session.config.id'].values[0],
             'trial_payoff': subject_row[f'ultimatum_game.{round}.player.payoff'].values[0],
-            'round_nb': subject_row[f'ultimatum_game.{round}.player.round_nb'].values[0],
+            'round_nb': subject_row[f'ultimatum_game.{round}.player.round_nb'].values[0],  
             'role': subject_row[f'ultimatum_game.{round}.player.player_role'].values[0],
             'player': subject_row['participant.id_in_session'].values[0],
             'dyad': subject_row[f'ultimatum_game.{round}.player.dyad'].values[0],
-            'manipulation': subject_row[f'ultimatum_game.{round}.player.is_dom'].values[0],
+            'manipulation': subject_row[f'ultimatum_game.{round}.player.manipulation'].values[0],
             'responded': subject_row[f'ultimatum_game.{round}.player.responded'].values[0],
             'sent_amount': subject_row[f'ultimatum_game.{round}.group.sent_amount'].values[0],
             'offer_response': subject_row[f'ultimatum_game.{round}.group.responder_accepted'].values[0],
@@ -58,7 +60,7 @@ for subject in raw_data['participant.id_in_session']:
 
     row_data_debrief = {
         'participant_code': subject_row['participant.code'].values[0],
-        'session_code': subject_row['session.conde'].values[0],
+        'session_code': subject_row['session.code'].values[0],
         'prolific_id': subject_row['ultimatum_game.20.player.prolific_id'].values[0],
         'sid': subject_row['session.config.name'].values[0],
         'mk_session': subject_row['session.config.id'].values[0],
@@ -84,8 +86,8 @@ for subject in raw_data['participant.id_in_session']:
         
 # %%
 
-clean_data.to_csv('raw_data\clean_data.csv', index=False)
-clean_data_debrief.to_csv('raw_data\clean_data_debrief.csv', index=False)
+clean_data.to_csv(f'clean_data\clean_{data_name}.csv', index=False)
+clean_data_debrief.to_csv(f'clean_data\clean_{data_name}_debrief.csv', index=False)
 
 
           
